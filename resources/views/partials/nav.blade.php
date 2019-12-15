613f91f20869675ca21db636e6c127c0cd757ec9
claudesimo <claudesimo1990@gmail.com>
2019-12-15 14:19:25 +0100

    init template

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,29 @@
+<nav class="fh5co-nav" role="navigation">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-xs-2">
+                <div id="fh5co-logo"><a href="index.html">GOauBled.</a></div>
+            </div>
+            <div class="col-md-6 col-xs-6 text-center menu-1">
+                <ul>
+                    <li class="has-dropdown">
+                        <a href="product.html">Test1</a>
+                        <ul class="dropdown">
+                            <li><a href="single.html">Test2</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about.html">Test3</a></li>
+                    <li class="has-dropdown active">
+                        <a href="services.html">Test4</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Web Design</a></li>
+                            <li><a href="#">eCommerce</a></li>
+                            <li><a href="#">Branding</a></li>
+                            <li><a href="#">API</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.html">Test5</a></li>
+                </ul>
+            </div>
+        </div>
+</nav>
